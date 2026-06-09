980412652004b86393a17adec365e45b69cfda1f
PedroEdur <145567971+PedroEdur@users.noreply.github.com>
2026-06-09 10:52:14 -0300

    ....

--- pedra, papel e tesoura/Assets/scripts/MainMenu.cs
+++ pedra, papel e tesoura/Assets/scripts/MainMenu.cs
@@ -10,6 +10,12 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
+        Debug.Log("Saindo do jogo...");
+
         Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
